a5652844f677ed60d4c9b77b9e10f6552f3f7e8d
Prashant Sachan <p-prashantsachan@goodearth.in>
2024-12-10 22:36:50 +0530

    fix error issue

--- src/index.server.js
+++ src/index.server.js
@@ -16,7 +16,7 @@ app.use("/api/user", userRouter); // Mount user routes
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true, // Optional but recommended for older MongoDB versions
+    // useNewUrlParser: true, // Optional but recommended for older MongoDB versions
     useUnifiedTopology: true,
   })
   .then(() => {
